afe32fe7452a53892da9363437ba4119652551c5
Adeel Ahmed <adeelahmed@Adeels-MacBook-Pro.local>
2017-10-12 19:29:45 -0500

    pseduocoded

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// ready fxn to let site know to start only when DOM is loaded up 
+
+/*random target number that player has to reach. something like math.floor(math.random)* whatever
+should be between 20 and 120 i think */
+//wins and losses variables
+
+//function to generate random values for each crystal
+
+/*function to reset game after finish
+so need to make score 0 
+gen random target number*/
+
+// if else statements for win/lose. show respective win/loss message and reset 
+
+//make win loss display and show on page
+
+//make crystals appear on page
+
+//fxn to make number add to current gues number each time crystal clicked
+
+//also fxn to make current guess appear on page
+
+//if else statement to check if target score reached. somethig like if currentGuess === targetScore then +1 wins. 
+//else currentGuess > target then +1 losses
+
+// make 
+
+
+//on clicks for crystals
+
+
+
+
+
